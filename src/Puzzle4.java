4743d2763a5f66d298c27f1a0793afec017daa28
Mario Fredenhagen <mario@fredenhagen.net>
2018-02-26 22:21:18 +0100

    day 4

---
+++ src/Puzzle4.java
@@ -0,0 +1,24 @@
+import java.util.function.Function;
+
+public class Puzzle4 {
+    public static void main(String[] args) {
+        System.out.println(solve(args));
+    }
+
+    protected static long solve(String[] args) {
+        return streamArgs(args, s -> s);
+    }
+
+    protected static long solveA(String[] args) {
+        return streamArgs(args, s -> new String(s.codePoints().sorted().toArray(), 0, s.length()));
+    }
+
+    private static long streamArgs(String[] args, Function<? super String, ? extends String> mapper) {
+        return java.util.Arrays.stream(args, 0, args.length).filter(str -> {
+            String[] split = str.split(" ");
+            return java.util.Arrays.stream(split, 0, split.length)
+                    .map(mapper)
+                    .distinct().count() == split.length;
+        }).count();
+    }
+}
