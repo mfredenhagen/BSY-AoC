d7fa72554de07f78bf9858f8731deb5c05aef9aa
Mario Fredenhagen <mario@fredenhagen.net>
2018-02-26 22:31:17 +0100

    day 4, added output from second part to main

--- src/Puzzle4.java
+++ src/Puzzle4.java
@@ -3,6 +3,7 @@ import java.util.function.Function;
 public class Puzzle4 {
     public static void main(String[] args) {
         System.out.println(solve(args));
+        System.out.println(solveA(args));
     }
 
     protected static long solve(String[] args) {
@@ -13,7 +14,7 @@ public class Puzzle4 {
         return streamArgs(args, s -> new String(s.codePoints().sorted().toArray(), 0, s.length()));
     }
 
-    private static long streamArgs(String[] args, Function<? super String, ? extends String> mapper) {
+    private static long streamArgs(String[] args, Function<String, String> mapper) {
         return java.util.Arrays.stream(args, 0, args.length).filter(str -> {
             String[] split = str.split(" ");
             return java.util.Arrays.stream(split, 0, split.length)
