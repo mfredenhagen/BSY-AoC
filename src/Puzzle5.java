169598815bde206db090960ea0327e909c1b53e2
Mario Fredenhagen <mario@fredenhagen.net>
2018-02-28 23:06:21 +0100

    day 5

---
+++ src/Puzzle5.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class Puzzle5 {
+    public static void main(String[] args) {
+        System.out.println("" + solve(args));
+        System.out.println("" + solveA(args));
+    }
+
+    protected static int solve(String[] args) {
+        return calc(args, i -> ++i);
+    }
+
+    protected static int solveA(String[] args) {
+        return calc(args, i -> i + (i >= 3 ? -1 : 1));
+    }
+
+    private static int calc(String[] args, Function<Integer, Integer> block) {
+        int[] jumps = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+        int current = 0, hops = 0;
+        while (current < jumps.length) {
+            int dist = jumps[current];
+            jumps[current] = block.apply(dist);
+            current += dist;
+            hops++;
+        }
+        return hops;
+    }
+}
